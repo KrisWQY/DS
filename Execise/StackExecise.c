746136b0c5572761c211fa8f20a35644e0cbedcb
KrisWQY <429680471@qq.com>
2019-10-17 20:45:49 +0800

    Add Execise folder and files.

---
+++ Execise/StackExecise.c
@@ -0,0 +1,46 @@
+
+
+VOID
+ConvertDec2Oct (
+	UINT32	Dec
+	)
+{
+	SqStack	*S;
+
+	Stack_Init(S);
+	while (Dec) {
+		Stack_Push(S, Dec % 8);
+	}
+
+	while ()
+
+}
+
+VOID
+EditLine (
+	VOID
+	) 
+{
+	SqStack S;
+	Stack_Init(&S);
+	ch = getchar();
+	while (ch != EOF) {
+		while (ch != EOF &&ch != '\n') {
+			switch (ch){
+				case '#':Pop(S, c); break;
+				case '@':Stack_Clear(&S); break;
+				default:break;   
+			}
+
+			
+		}
+	}
+}
+
+DS_STATUS
+CaculateMazePath (
+
+	)
+{
+
+}
