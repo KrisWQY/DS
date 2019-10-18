140a74e987cd9441d6f1734a585b08919b9a4475
KrisWQY <429680471@qq.com>
2019-10-18 21:35:04 +0800

    Add DS main file.

--- DS.c
+++ DS.c
@@ -1,32 +1,58 @@
-#include <stdio.h>
+#include "Include/DS.h"
 
-int maxSubArray(int *nums, int numsSize)
-{
-    int CurSum = 0;
-    int MaxSum = nums[0];
-    int i;
-    for (i = 0; i < numsSize; i++)
-    {
-        CurSum += nums[i];
-        if (CurSum > MaxSum)
-        {
-            MaxSum = CurSum;
-        }
-        else if (CurSum < 0)
-        {
-            CurSum = 0;
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+int thirdMax(int* nums, int numsSize){
+    int result[3] = {0};
+    int idx;
+    int Flag = 0;
+    
+    switch (numsSize) {
+        case 1: return nums[0];
+            break;
+        case 2: return MAX(nums[0], nums[1]);
+            break;
+    }
+    
+    if (nums[0] > nums[1]) {
+        result[0] = nums[0];
+        result[1] = nums[1];
+    } else if (nums[0] < nums[1]) {
+        result[0] = nums[1];
+        result[1] = nums[0];
+    } else {
+        result[0] = nums[0];
+    }
+    
+    for (idx = 2; idx < numsSize; idx++) {
+        if (nums[idx] > result[0]) {
+            result[2] = result[1];
+            result[1] = result[0];
+            result[0] = nums[idx]; 
+        } else if (nums[idx] > result[1] && nums[idx] != result[0]) {
+            result[2] = result[1];
+            result[1] = nums[idx];
+            Flag = 1;
+        } else if (nums[idx] > result[2] && nums[idx] != result[1] && nums[idx] != result[0]) {
+            result[2] = nums[idx];
+            Flag = 1;
         }
     }
-
-    return MaxSum;
+    
+    if (result[0] != result[1] && result[1] != result[2] && Flag != 0) {
+        return result[2];
+    } else {
+        return result[0];
+    }
 }
 
 int main()
 {
-    int nums[] = {-3, -2, 0, -1};
+    int nums[] = {2, 2, 2, 1};
     int result;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
 
-    result = maxSubArray(nums, 4);
+    result = thirdMax(nums, numsSize);
     printf("result:%d\n", result);
     return 0;
 }
